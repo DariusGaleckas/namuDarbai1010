b9780a8d40276eae9b3f7cc764c6f04e133cf482
DariusGaleckas <dariusg5@gmail.com>
2023-10-13 04:54:15 +0300

    dev1

---
+++ script1010.js
@@ -0,0 +1,136 @@
+console.log('Labas pasauliYYY');
+
+//Pirma užduotis//
+console.log('PIRMA UŽDUOTIS');
+
+let stringas = 'Tomas'
+console.log(stringas.length)
+
+const a = 'Tomas';
+const b = 'Hanksas';
+
+console.log(a, b)
+console.log(a.length)
+console.log(b.length)
+
+if (a.length < b.length) {
+    console.log(`${a} is less than ${b}`);
+  } else if (a.length > b.length) {
+    console.log(`${a} is greater than ${b}`);
+  } else {
+    console.log(`${a} and ${b} are equal.`);
+  }
+//Antra užduotis//
+console.log('ANTRA UŽDUOTIS');
+
+  const Vardas = 'Dar';
+  const Pavardė = 'Gal';
+  const GimimoMetai = '1771';
+  const dataŠiandien = '2023';
+  const a1 = 'Aš esu'
+  const a2 ='. Man'
+  const a3 = 'metai.'
+  let x = dataŠiandien - GimimoMetai
+console.log('Mano vardas', Vardas, Pavardė, '. Man', x, 'metai');
+console.log(a1, Vardas, Pavardė, a2, x, a3)
+const viskas = `Mano vardas ${Vardas} ${Pavardė}. Man ${x} metai.`;
+console.log(viskas)
+
+//Trečia užduotis//
+console.log('TREČIA UŽDUOTIS');
+
+let stringas1 = 'Elizabeth';
+let stringas2 = 'HURLEY'
+console.log(stringas1, stringas2);
+console.log(stringas1[stringas1.length -1]);
+let b1 = stringas1[stringas1.length -3] 
+let b2 = stringas1[stringas1.length -2] 
+let b3 = stringas1[stringas1.length -1]
+let b4 = stringas2[stringas2.length -3]
+let b5 = stringas2[stringas2.length -2] 
+let b6 = stringas2[stringas2.length -1] 
+console.log(b1 + b2 + b3 + b4 + b5 + b6) 
+
+//Ketvirta užduotis//
+console.log('KETVIRTA UŽDUOTIS');
+
+
+let stringas3 = "Once upon a time in hollywwod";
+console.log(stringas3)
+stringas3 = stringas3.replaceAll("o", "*");
+console.log(stringas3)
+stringas3 = stringas3.replaceAll("O", "*");
+console.log(stringas3)
+
+//penkta užduotis//
+console.log('PENKTA UŽDUOTIS');
+
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let sk1 = rand(0,2)
+let sk2 = rand(0,2)
+let sk3 = rand(0,2)
+let sk4 = rand(0,2)
+let nuliai
+
+console.log(sk1, sk2, sk3, sk4);
+
+
+//if (sk1 === 0) console.log {sk1}//
+
+
+//šešta užduotis//
+console.log('ŠEŠTA UŽDUOTIS');
+
+//Pasinaudokite atsitiktinio skaičiaus generavimo funkcija.
+// Sukurkite du kintamuosius ir naudodamiesi funkcija jiems priskirkite atsitiktines reikšmes nuo 0 iki 4. //
+//Didesnę reikšmę padalinkite iš mažesnės.
+//Atspausdinkite rezultatą jį suapvalinę iki 2 skaičių po kablelio.//
+
+let skk1 = rand(0,4)
+let skk2 = rand(0,4)
+console.log(skk1, skk2);
+//let
+//if(skk1 < skk2) {console.log(skk1 / skk2)
+
+
+
+
+
+//Septinta užduotis//
+console.log('SEPTINTA UŽDUOTIS');
+
+
+//Aštunta užduotis
+console.log('AŠTUNTA UŽDUOTIS');
+
+
+
+
+//Devinta užduotis//
+//Parašyti kodą, kuris generuotų atsitiktinį stringą iš lotyniškų mažųjų raidžių. Stringo ilgis 3 simboliai
+console.log('DEVINTA UŽDUOTIS');
+
+
+ //let a1b = `a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,z`;
+ //console.log(a1b);
+ //a1b = a1b.replaceAll(",", "");
+//console.log(a1b)
+//console.log(a1b.length)
+///let a1b1 = a1b[a1b.length 4] 
+//console.log(a1b1)
+//console.log()
+//let b1 = stringas1[stringas1.length -3] 
+
+
+
+//console.log[rand(0,25)]
+
+//let a1b1 = a1b1[a1b1.length3] 
+//console.log(a1b1)
+
+
